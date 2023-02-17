5992b5d11ce72355b595d87408974c0b8b452785
borys <borysdarecki121@gmail.com>
2023-02-17 12:45:03 +0100

    jebie mnie to tak szczrze

--- js/slider.js
+++ js/slider.js
@@ -32,7 +32,7 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("kierunek");
   if (slideIndex==slides.length) {slideIndex=0}
-  if (n > slides.length) {slideIndex = 0}
+  if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
