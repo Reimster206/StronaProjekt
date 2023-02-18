41082f4d4f79df2f2a9b265bb210ec8f78f8bdca
borys <borysdarecki121@gmail.com>
2023-02-18 16:20:56 +0100

    dziala

--- js/galeria.js
+++ js/galeria.js
@@ -1,11 +1,12 @@
-var fullImgBox = document.querySelectorAll('.zdj-post')
-var fullImg = document.getElementById("fullImg");
 
 function openImg(n) {
+    let fullImgBox = document.querySelector('.full-img')
+    let fullImg = document.getElementById("fullImg");
     fullImgBox.style.visibility="visible";
     fullImg.src="img/zdj"+n+".jpg";
 }
 
 function closeImg(){
+    let fullImgBox = document.querySelector('.full-img');
     fullImgBox.style.visibility="hidden";
 }
