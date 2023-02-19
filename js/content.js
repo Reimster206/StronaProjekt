cd9cd31537fa58b1d2771c6b12df5f4e5c471db3
borys <borysdarecki121@gmail.com>
2023-02-19 21:46:21 +0100

    sraczka

--- js/content.js
+++ js/content.js
@@ -1,13 +1,67 @@
-function wiecej(n){
-    let d = document.querySelectorAll(".wiecej");
-    let p = document.querySelectorAll(".przycisk1");
-    let a = document.querySelectorAll(".to");
+var prgb = document.querySelector(".kierunek-prg");
+    var infb = document.querySelector(".kierunek-inf");
+    var mechb = document.querySelector(".kierunek-mech");
+    var robb = document.querySelector(".kierunek-rob");
+    var autb = document.querySelector(".kierunek-aut");
+    var eleb = document.querySelector(".kierunek-ele");
 
-    if(d[n].style.display == "inline"){
-        d[n].style.display = "none";
-        p[n].innerHTML = "czytaj wiecej";
-    }else{
-        d[n].style.display = "inline";
-        p[n].innerHTML = "czytaj mniej";
+    function reset(){
+        prgb.style.display="none";
+        infb.style.display="none";
+        mechb.style.display="none";
+        robb.style.display="none";
+        autb.style.display="none";
+        eleb.style.display="none";
     }
-}
+
+    function prg(){
+        prgb.style.display="block";
+        infb.style.display="none";
+        mechb.style.display="none";
+        robb.style.display="none";
+        autb.style.display="none";
+        eleb.style.display="none";
+    }
+
+    function inf(){
+        prgb.style.display="none";
+        infb.style.display="block";
+        mechb.style.display="none";
+        robb.style.display="none";
+        autb.style.display="none";
+        eleb.style.display="none";
+    }
+    function mech(){
+        prgb.style.display="none";
+        infb.style.display="none";
+        mechb.style.display="block";
+        robb.style.display="none";
+        autb.style.display="none";
+        eleb.style.display="none";
+    }
+
+    function rob(){
+        prgb.style.display="none";
+        infb.style.display="none";
+        mechb.style.display="none";
+        robb.style.display="block";
+        autb.style.display="none";
+        eleb.style.display="none";
+    }
+    function aut(){
+        prgb.style.display="none";
+        infb.style.display="none";
+        mechb.style.display="none";
+        robb.style.display="none";
+        autb.style.display="block";
+        eleb.style.display="none";
+    }
+
+    function ele(){
+        prgb.style.display="none";
+        infb.style.display="none";
+        mechb.style.display="none";
+        robb.style.display="none";
+        autb.style.display="none";
+        eleb.style.display="block";
+    }
